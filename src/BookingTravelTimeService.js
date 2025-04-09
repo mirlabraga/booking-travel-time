0b86a296e361d9973ac1beba41ef42b5731c3ee3
Mirla Braga <mirla.braga@gmail.com>
2025-04-09 14:56:01 +0100

    Created Validations for year, day, hour and time for travel with clean message for error

--- src/BookingTravelTimeService.js
+++ src/BookingTravelTimeService.js
@@ -7,22 +7,33 @@ function bookTravelTime(date) {
     const ticketTravelTime = new TicketTravelTime();
 
     if (date) {
-        throw new TravelTimeServiceExpection("[ERROR]: A error happend when book tavel time was executed");
+        throw new TravelTimeServiceExpection("[ERROR]: A error happend when book tavel time was executed.");
     }
 
     const year = date.year;
     const day = date.day;
     const hour = date.hour;
     const time = date.time;
-
-    if(IsValidateSelectedYear(year) 
-        && IsValidateSelectedDay(day) 
-        && IsValidateSelectedHour(hour)
-        && IsValidateSelectedTime(time)) { 
-        ticketTravelTime = {  
-
-        }
+    
+    if(!IsValidateSelectedYear(year)) {
+        throw new TravelTimeServiceExpection("[ERROR]: This YEAR IS NOT ALLOW to travel.");
     }
+
+    if(!IsValidateSelectedDay(day)) {
+        throw new TravelTimeServiceExpection("[ERROR]: This DAY IS NOT ALLOW to travel.");
+    }
+
+    if(!IsValidateSelectedHour(hour)) {
+        throw new TravelTimeServiceExpection("[ERROR]: This HOUR IS NOT ALLOW to travel.");
+    }
+
+    if(!IsValidateSelectedTime(time)) {
+        throw new TravelTimeServiceExpection("[ERROR]: This TIME IS NOT ALLOW to travel.");
+    }
+
+    ticketTravelTime = {  
+    }
+
     return ticketWithReturn;
 }
 
--- src/ValitionBookingTravelTimeService.js
+++ src/ValitionBookingTravelTimeService.js
@@ -7,4 +7,19 @@ const END_YEAR = 2021;
 function IsValidateSelectedYear(year) {
   return year >= START_YEAR && year <= END_YEAR;
 }
+
+function IsValidateSelectedDate(date) {
+}
+
+function IsValidateSelectedHour(hour) {
+}
+
+function IsValidateSelectedTime(time) {
+
+}
+
 module.exports = IsValidateSelectedYear;
+module.exports = IsValidateSelectedDate;
+module.exports = IsValidateSelectedHour;
+module.exports = IsValidateSelectedTime;
+
