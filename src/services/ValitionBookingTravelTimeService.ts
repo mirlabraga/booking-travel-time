24352dba4d0a701ffbc9476ca238af287ddba1a5
Mirla Braga <mirla.braga@gmail.com>
2025-04-20 11:37:09 +0100

    Adding parameter in the validation for query validad travel day

--- src/services/ValitionBookingTravelTimeService.ts
+++ src/services/ValitionBookingTravelTimeService.ts
@@ -1,40 +1,34 @@
-// const START_YEAR = env.process.START_YEAR || 1990;
-// const END_YEAR = env.process.END_YEAR || 2021;
+const YEAR_FOR_TRAVEL = process.env.START_YEAR || 2024;
 
-const START_YEAR = 1990;
-const END_YEAR = 2021;
+export class ValitionBookingTravelTimeService {
+  
+  IsValidate(date: { year: any; day: any; hour: any; time: any; }, traveler: Traveler) {
 
-function IsValidate(date: string | undefined, person: string | undefined) {
-  IsValidateSelectedYear(date);
-  IsValidateSelectedDate(date);
-  IsValidateSelectedHour(date);
-  IsValidateSelectedTime(date);
-  IsValidateDNAPerson(person);
+    this.IsValidateSelectedYear(date.year);
+    this.IsValidateSelectedDay(date.day);
+    this.IsValidateSelectedHour(date.hour, {});
+    this.IsValidateSelectedTime(date.time, {});
+    this.IsValidateDNAPerson(traveler);
+  }
+  
+  IsValidateSelectedYear(year: number) {
+    return year >= Number(YEAR_FOR_TRAVEL);
+  }
+  
+  IsValidateSelectedDay(day: number) {
+    //return year >= START_YEAR && year <= END_YEAR;
+    //getValidDay()
+  }
+  
+  IsValidateSelectedHour(hour: number, { }) {
+    // return getInvalidHoursForYearDay()
+  }
+  
+  IsValidateSelectedTime(time: number, {}) {
+    // return year >= START_YEAR && year <= END_YEAR;
+    // TO DEFENID YET
+  }
+  
+  IsValidateDNAPerson(traveler: Traveler) {
+  }
 }
-
-function IsValidateSelectedYear(year) {
-  return year >= START_YEAR && year <= END_YEAR;
-}
-
-function IsValidateSelectedDate(date) {
-  return year >= START_YEAR && year <= END_YEAR;
-}
-
-function IsValidateSelectedHour(hour) {
-  return year >= START_YEAR && year <= END_YEAR;
-}
-
-function IsValidateSelectedTime(time) {
-  return year >= START_YEAR && year <= END_YEAR;
-}
-
-function IsValidateDNAPerson(person) {
-}
-
-module.exports = IsValidate;
-module.exports = IsValidateDNAPerson;
-module.exports = IsValidateSelectedYear;
-module.exports = IsValidateSelectedDate;
-module.exports = IsValidateSelectedHour;
-module.exports = IsValidateSelectedTime;
-
--- tests/ValitionBookingTravelTimeService.test.ts
+++ tests/ValitionBookingTravelTimeService.test.ts
@@ -1,12 +1,15 @@
-// import IsValidateSelectedYear from '../src/Validation';
+// import { ValitionBookingTravelTimeService } from "../src/services/ValitionBookingTravelTimeService";
+
+import { ValitionBookingTravelTimeService } from "../src/services/ValitionBookingTravelTimeService";
 
 test('validate that selected year is in the not restrict period', () => {
-  // expect(IsValidate(2015)).toBe(true);
+  const valitionBookingTravelTimeService: ValitionBookingTravelTimeService = new ValitionBookingTravelTimeService();
+  // expect(valitionBookingTravelTimeService.IsValidateSelectedYear(2015)).toBe(true);
 });
 
-test('validate that selected year is in the not restrict period', () => {
-  // expect(IsValidateSelectedYear(2015)).toBe(true);
-  // expect(IsValidateSelectedYear(2015)).toBe(true);
-  // expect(IsValidateSelectedYear(2015)).toBe(true);
-  // expect(IsValidateSelectedYear(2015)).toBe(true);
-});
+// test('validate that selected year is in the not restrict period', () => {
+//   // expect(IsValidateSelectedYear(2015)).toBe(true);
+//   // expect(IsValidateSelectedYear(2015)).toBe(true);
+//   // expect(IsValidateSelectedYear(2015)).toBe(true);
+//   // expect(IsValidateSelectedYear(2015)).toBe(true);
+// });
